f61d4dfe871782d3f6e603f8b2e73b9759226961
Cypher Moon <temperaturecaleb@gmail.com>
2022-12-22 18:08:58 +0100

    created link list component

---
+++ src/components/LinkList.jsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react'
+import Link from './Link';
+
+const LinkList = () => {
+    const FEED_QUERY = gql`
+         {
+            feed {
+               id
+               links {
+                    id
+                    createdAt
+                    url
+                    description
+               }
+            }
+        }
+    `
+
+    const { data } = useQuery(FEED_QUERY)
+
+    return (
+        <div>
+            {data && data.feed.links.map((link) => <Link key={link.id} link={link} />)}
+        </div>
+    )
+}
+
+export default LinkList
